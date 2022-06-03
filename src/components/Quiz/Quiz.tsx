9e07fe75abeefa0e8f9932d9c42ad2231aaf3eea
vuonga1103 <vuonga1103@yahoo.com>
2022-06-03 17:29:19 -0400

    Get current quote with characters

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -1,21 +1,30 @@
 import React from 'react'
+import { QuoteState } from '../QuizContainer'
 import QuestionCard from './QuestionCard'
 
 type Props = {
-  startQuiz: () => void
-  checkAnswer: (e: React.MouseEvent<HTMLButtonElement>) => void
-  getNextQuestion: () => void
+  // startQuiz: () => void
+  // checkAnswer: (e: React.MouseEvent<HTMLButtonElement>) => void
+  // getNextQuestion: () => void
+  currentQuote: QuoteState | null
 }
 
 const TITLE = 'Welcome to Who is Justice Beaver?'
 const SUBTITLE = 'An Office Quiz Game'
 
-const Quiz: React.FC<Props> = ({ startQuiz, checkAnswer, getNextQuestion }) => {
+// quiz should receive
+// current place in quiz ex. 1/10
+// question, along with answers and correct answers
+const Quiz: React.FC<Props> = ({
+  currentQuote,
+  //  startQuiz, checkAnswer, getNextQuestion
+}) => {
+  console.log(currentQuote)
   return (
     <>
       <h1>{TITLE}</h1>
       <h2>{SUBTITLE}</h2>
-      <button className="start" onClick={startQuiz}>
+      <button className="start" onClick={() => {}}>
         Start:
       </button>
       <p className="score">Score:</p>
--- src/components/QuizContainer.tsx
+++ src/components/QuizContainer.tsx
@@ -17,17 +17,45 @@ const getNextQuestion = () => {}
 
 export const ERROR_MESSAGE = 'Something went wrong!'
 
+export type QuoteState = Quote & {
+  characters: Character[]
+}
+
+const getCharacters = (
+  characters: Character[],
+  correctCharacter: Character
+) => {
+  const charactersExcludingCorrectCharacter = characters.filter(
+    ({ _id }) => _id !== correctCharacter._id
+  )
+  const randomThreeCharacters = _.shuffle(
+    charactersExcludingCorrectCharacter
+  ).slice(0, 3)
+  return _.shuffle([correctCharacter, ...randomThreeCharacters])
+}
+
+const getCurrentQuoteWithCharacters = (
+  quote: Quote,
+  characters: Character[]
+) => {
+  if (!quote) return null
+
+  const { character: correctCharacter } = quote
+  return { ...quote, characters: getCharacters(characters, correctCharacter) }
+}
+
 const QuizContainer = () => {
   const [quotes, setQuotes] = useState<Quote[] | []>([])
   const [characters, setCharacters] = useState<Character[] | []>([])
   const [error, setError] = useState<string | null>(null)
+  const [currentQuoteIdx, setCurrentQuoteIdx] = useState<number>(0)
 
   useEffect(() => {
     const getQuotes = async () => {
       return await axios
         .get(QUOTES_API_URL)
         .then<Quote[]>(getResponseData)
-        .then(data => setQuotes(data))
+        .then(data => setQuotes(_.shuffle(data)))
         .catch(error => setError(error.message))
     }
 
@@ -48,11 +76,19 @@ const QuizContainer = () => {
     return <h1>{ERROR_MESSAGE}</h1>
   }
 
+  const currentQuote = getCurrentQuoteWithCharacters(
+    quotes[currentQuoteIdx],
+    characters
+  )
+
   return (
     <Quiz
-      startQuiz={startQuiz}
-      checkAnswer={checkAnswer}
-      getNextQuestion={getNextQuestion}
+      currentQuote={currentQuote}
+      // startQuiz={startQuiz}
+      // checkAnswer={checkAnswer}
+      // getNextQuestion={getNextQuestion}
+      // // currentQuote={quotes[currentQuoteIdx]}
+      // // currentQuoteIdx={currentQuoteIdx}
     />
   )
 }
